4f7f1c14fba314d3acb0f3970fc4c5c70cb3b5aa
vlad <jan+code@alumonium.com>
2024-04-14 01:14:21 +0200

    facebook filter

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -1,39 +1,54 @@
-inside_main = (node) => {
+inside_main = (node, count = 0) => {
     if (!node) {
-        return false
+        return 0
     } else if (node.role === "main") {
-        return true
+        return count
     } else {
-        return inside_main(node.parentNode)
+        return inside_main(node.parentNode, count + 1)
     }
+}
+
+remove_node = (node) => {
+    // node.style.display = "none";
+    node.style.border = "1px solid #1c1e21;";
+    node.childNodes.forEach((node) => {
+        node.style.display = "none";
+    });
 }
 
 new MutationObserver((mutations) => {
     for (const mutation of mutations) {
         if (mutation.type === "childList") {
             for (let node of mutation.addedNodes) {
-                if (node.nodeName === "DIV" && node.className.contains("html-div")) {
-                    node.style.border = "1px solid red";
-                   //if (inside_main(node)) {
-                   //     let text = node.textContent.trim()
-                   //      if (text.contains("Follow")) {
-                   //          node.style.border = "1px solid red";
-                   //      }
-                   //      if (text.contains("Reels")) {
-                   //          node.style.border = "1px solid blue";
-                   //      }
-                   //}
+                if (node.nodeName === "DIV") {
+                    let count = inside_main(node)
+                    if (count === 9) {
+                        let text = node.textContent.trim()
+                        if (text.contains("here are groups you might like")) {
+                            node.style.border = "1px solid pink";
+                            remove_node(node);
+                        } else if (text.contains("Follow")) {
+                            node.style.border = "1px solid red";
+                            remove_node(node);
+                        } else if (text.contains("Suggested for you")) {
+                            node.style.border = "1px solid blue";
+                            remove_node(node);
+                        } else if (text.contains("Join")) {
+                            node.style.border = "1px solid green";
+                            remove_node(node);
+                        } else if (text.contains("Reels and short videos")) {
+                            node.style.border = "1px solid yellow";
+                            remove_node(node);
+                        }
+                    }
                 }
             }
-        } else if (mutation.type === "attributes") {
-            console.log(mutation.target)
         }
-
     }
 }).observe(
     document,
     {
-        attributes: true,
+        attributes: false,
         childList: true,
         subtree: true
     }
