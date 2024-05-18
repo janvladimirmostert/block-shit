5e275edb25d194a1a3d9a8545cc350399771509e
vlad <jan+code@alumonium.com>
2024-05-18 10:44:10 +0200

    working on block counters

--- background.js
+++ background.js
@@ -12,23 +12,23 @@ let badger = (text) => {
 
 badger(":-)")
 
-// Example of a simple user data object
-const user = {
-    username: 'demo-user'
-};
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    // 2. A page requested user data, respond with a copy of `user`
-    if (message === 'get-user-data') {
-        sendResponse(user);
-    }
-    badger("!!!");
-    return true; //for async reasons i dont get either
-
-});
-
-handleActionClick = () => {
-    badger("???");
-}
-
-chrome.action.onClicked.addListener(handleActionClick);
+// // Example of a simple user data object
+// const user = {
+//     username: 'demo-user'
+// };
+//
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//     // 2. A page requested user data, respond with a copy of `user`
+//     if (message === 'get-user-data') {
+//         sendResponse(user);
+//     }
+//     badger("!!!");
+//     return true; //for async reasons i dont get either
+//
+// });
+//
+// handleActionClick = () => {
+//     badger("???");
+// }
+//
+// chrome.action.onClicked.addListener(handleActionClick);
